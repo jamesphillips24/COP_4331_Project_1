b4c6524acbca9eb2c8469d417366dfd1a691cc41
= <sethjonesintern@gmail.com>
2025-08-19 23:07:35 -0400

    Initial commit with buttons kind of working

---
+++ Frontend/js/app.js
@@ -0,0 +1,37 @@
+const signupBtn = document.getElementById("signupBtn");
+const loginBtn = document.getElementById("loginBtn");
+
+if(signupBtn){
+    signupBtn.addEventListener("click", function(){
+    const name = document.getElementById("name").value;
+    const email = document.getElementById("email").value;
+    const username = document.getElementById("username").value;
+    const password = document.getElementById("password").value;
+    const confirmPass = document.getElementById("confirmPassword").value;
+
+    const signupData = {
+        name: name,
+        email: email,
+        username: username,
+        password: password,
+        confirmPass: confirmPass
+    };
+
+    console.log(signupData);
+    });
+}
+if (loginBtn){
+    loginBtn.addEventListener("click", function(){
+
+        const username = document.getElementById("username").value;
+        const password = document.getElementById("password").value;
+
+
+        const loginData = {
+            username: username,
+            password: password
+        };
+
+        console.log(loginData);
+    });
+}
