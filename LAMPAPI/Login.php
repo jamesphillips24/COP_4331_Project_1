caa6e348c7b28a706e6bda302a969e53cbe6fcfa
EclipsedStar <83985361+EclipsedStar0@users.noreply.github.com>
2025-08-23 21:23:01 -0400

    Login PHP
    -Created LAMPAPI Folder
    -Created Login.php with four functions:
    --attemptLogin() makes a database connection and checks if the username/password combination is correct, passing on the userID/firstName/lastName if it is
    --decodeJSON does its name, converting json to a dictionary
    --returnWIthInfo() returns as a dictionary, the id/firstName/lastName/errorCode.
    --sendResultInfoAsJson converts the dictionary created by returnWIthInfo to Json.
    --placeholder database credentials have been set as 'masterUserName'/'masterPassword' with the database fetched called 'COP4331_Project1' for now.

---
+++ LAMPAPI/Login.php
@@ -0,0 +1,49 @@
+<?php
+	$inputData = decodeJSON();
+	$apiKeyUsername = "masterUserName";
+	$apiKeyPassword = "masterPassword";
+	$databaseName = "COP4331_Project1";
+	$connection = new mysqli("localhost", $apiKeyUsername, $apiKeyPassword, $databaseName);
+	attemptLogin($inputData["username"], $inputData["password"], $connection);
+	
+	function attemptLogin($username, $password, $providedConnection) {
+		$sqlCMD = $providedConnection->prepare("SELECT UserID, firstName, lastName FROM PasswordDB WHERE Username=? AND Password=?");
+		$sqlCMD->bind_param("ss", $username, $password);
+		$sqlCMD->execute();
+		$rowHolder = $sqlCMD->get_result();
+		$index = 0;
+		$result = "";
+		while($rowInfo = $rowHolder->fetch_assoc()) {
+			if( $index > 0 ) {
+				$result .= ",";
+			}
+			$index++;
+			$result .= '"' . $rowInfo["UserID"] . '"';
+		}
+		if ($index == 0) {
+			returnWithInfo("", "", "", "Invalid User/Password combination");
+		}
+		else {
+			returnWithInfo($rowInfo["userID"], rowInfo["firstName"], rowInfo["lastName"], "");
+		}
+		$sqlCMD->close();
+		$providedConnection->close();
+	}
+
+
+	function decodeJSON()
+	{
+		return json_decode(file_get_contents('php://input'), true);
+	}
+	
+	function returnWithInfo( $userID, $firstName, $lastName, $error ) {
+		$returnValue = '{"id":' . $id . ',"firstName":"' . $firstName . '","lastName":"' . $lastName . '","error":' . $error . '}';
+		sendResultInfoAsJson( $returnValue );
+	}
+	
+	function sendResultInfoAsJson( $obj ) {
+		header('Content-type: application/json');
+		echo $obj;
+	}
+
+?>
